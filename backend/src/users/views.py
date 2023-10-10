8841746a7a67a7ec69c52ba787614b1786f52c2b
Ananyobrata Pal <ananyo141@gmail.com>
2023-10-11 04:36:29 +0530

    fix(backend): remove same site altogether

--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -87,7 +87,6 @@ class Login(TokenObtainPairView):
                 value=accessToken,
                 expires=86400,  # 1 day
                 httponly=True,
-                samesite="None",  # 'Lax' or 'Strict
                 secure=True,
             )
 
